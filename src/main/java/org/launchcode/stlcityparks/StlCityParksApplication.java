60b0c05858156e4df388611228838d7771c71dcb
LAnderson-STL <kirsch_17@hotmail.com>
2019-06-07 13:58:36 -0500

    Create Park class

---
+++ src/main/java/org/launchcode/stlcityparks/StlCityParksApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.stlcityparks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StlCityParksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StlCityParksApplication.class, args);
+	}
+
+}
